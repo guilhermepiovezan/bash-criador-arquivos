71cafe8e8ea71fb24b52fa8f4abb0f6e0d67ec5e
Guilherme Piovezan <124106245+guilhermepiovezan@users.noreply.github.com>
2025-04-15 21:46:43 -0300

    Atualizar o criar_arquivos.sh

--- criar_arquivos.sh
+++ criar_arquivos.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Defina seu nome aqui
+# Definir meu nome aqui
 NOME="Guilherme"
 
-# Encontra o maior número já utilizado nos arquivos com o padrão
+# Encontrar o maior número já utilizado nos arquivos 
 ULTIMO_NUMERO=$(ls ${NOME}[0-9]* 2>/dev/null | grep -oP "${NOME}\K[0-9]+" | sort -n | tail -1)
 
 # Se não houver arquivos, começa do 1
@@ -11,15 +11,15 @@ if [ -z "$ULTIMO_NUMERO" ]; then
   ULTIMO_NUMERO=0
 fi
 
-# Calcula o próximo número inicial
+# Calcular o próximo número inicial
 INICIO=$((ULTIMO_NUMERO + 1))
 
-# Cria 25 arquivos com nomes sequenciais
+# Criar 25 arquivos com nomes sequenciais
 for ((i=0; i<25; i++)); do
   NUM=$((INICIO + i))
   touch "${NOME}${NUM}"
 done
 
-# Mostra o conteúdo do diretório
+# Mostrar o conteúdo do diretório
 ls -lh ${NOME}*
 
